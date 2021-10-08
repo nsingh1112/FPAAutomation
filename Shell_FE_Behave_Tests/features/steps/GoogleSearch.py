f4d4d3d27c2af889e1e62ee79829aae64f94df3d
Praveen Kumar <praveen.kumara@shell.com>
2021-10-08 20:18:37 +0530

    Initial commit
    Initial commit including INI file, code to read the Config file and sample test along with browser initiation

---
+++ Shell_FE_Behave_Tests/features/steps/GoogleSearch.py
@@ -0,0 +1,16 @@
+import sys
+from behave import *
+from Shell_FE_Selenium.SeleniumBase import SeleniumBase
+from pprint import pprint
+
+
+SeleniumBase.read_config()
+SeleniumBase.initialize_values()
+driver = SeleniumBase.browser_initialization()
+
+
+@Given('user navigates to "{text}"')
+def url_navigation(context, text):
+    driver.get(text)
+    pprint(sys.path)
+    driver.quit()
