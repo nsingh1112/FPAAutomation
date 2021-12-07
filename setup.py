79e3fb5431ea563428d397d87712304b2cb2af1f
Praveen Kumar <praveen.kumara@shell.com>
2021-12-07 18:27:43 +0530

    Updated browser

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 
 setuptools.setup(
     name="Shell_FE_Selenium_Core",
-    version='1.0.0a8',
+    version='1.0.0a9',
     author="Praveen Kumar Appusamy",
     author_email="praveen.kumara@shell.com",
     description="Selenium Core package to be used along with the Shell FE Behave framework for UI automation.",
