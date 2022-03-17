20f998c6c9ef68977bfaf2b8248baa5cc76d78b2
Praveen Kumar <praveen.kumara@shell.com>
2022-03-17 17:09:06 +0530

    Removed commented lines from setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,12 @@ import setuptools
 from setuptools import setup
 from setuptools import find_packages
 
-# with open('README.md') as f:
-#     long_description = f.read()
-
 setuptools.setup(
     name="Shell-FE-Selenium-Core",
     version='1.0.0a2',
     author="Praveen Kumar Appusamy",
     author_email="praveen.kumara@shell.com",
     description="Selenium Core package to be used along with the Shell FE Behave framework for UI automation.",
-    # long_description=long_description,
     long_description_content_type='text/markdown',
     license="Proprietary",
     classifiers=[
@@ -20,11 +16,5 @@ setuptools.setup(
         'Operating System :: OS Independent'
     ],
     packages=setuptools.find_packages(exclude=["Shell_FE_Behave_Tests"]),
-    # install_requires=[
-    #     'selenium',
-    #     'webdriver-manager',
-    #     'msedge-selenium-tools',
-    #     'openpyxl'
-    # ],
     python_requires='>=3.9'
 )
