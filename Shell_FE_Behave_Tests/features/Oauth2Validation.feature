5d51a1154a09dc6de5423406f082b6624f71e714
Mishra <Chinmaya.Mishra@shell.com>
2022-03-23 12:26:04 +0530

    Adding and modifying the files to support business layer

--- Shell_FE_Behave_Tests/features/Oauth2Validation.feature
+++ Shell_FE_Behave_Tests/features/Oauth2Validation.feature
@@ -2,4 +2,6 @@
 Feature: Validate OATH2 API with Requests
  @oathrequest
   Scenario: OATH2 Request validation
-    When user sends a POST request to retrieve details
+    When user sends a POST request to retrieve access token
+    Then user validates if the response status is "200"
+    Then user sends a GET request to access details
--- Shell_FE_Behave_Tests/features/steps/Oauth2Validation.py
+++ Shell_FE_Behave_Tests/features/steps/Oauth2Validation.py
@@ -1,36 +1,20 @@
 from behave import Given, When, Then
 from Shell_FE_Requests_Core.RequestsBase import RequestsBase
 from Shell_FE_Requests_Core.Utilities.AssertionUtilities import AssertionUtilities
+from Shell_FE_Behave_Tests.ApiApplicationLibrary.FunctionalLibrary.UsersApi import UsersApi
 
-@When(u'user sends a POST request to retrieve details')
+@When(u'user sends a POST request to retrieve access token')
 def step_impl(context):
-    """
-    User needs to provide mandatory details to retrieve access token and then use access token to raise another request
-    token_url = Url from which toke  needs to be retrieved
-    client_id = consumer key of your connected app
-    username = your login username for org
-    password =  your org login password
-    client_secret = consumer secret of your connected app
-    test_url = instance URL you need to make a request
-    :param context:
-    :return:
-    """
-    client_id = ""
-    client_secret = ""
-    token_url = ""
-    test_url = ""
-    username = ""
-    password = ""
+    context.feature.users_api = UsersApi()
+    context.access_token = context.feature.users_api.get_access_token()
 
-    data =  {'grant_type': 'password','username': username, 'password': password, 'client_id': client_id, 'client_secret': client_secret}
-    access_token = RequestsBase.get_access_token(url= token_url, data=data)
-    if access_token.status_code == 200:
-        access_token = access_token.json()["access_token"]
-        api_call_headers = {'Authorization': 'Bearer ' + access_token}
-        RequestsBase.get_request(url=test_url, headers=api_call_headers)
-        AssertionUtilities.assert_equals('200', str(RequestsBase.response_status_code()))
-    else:
-        AssertionUtilities.assert_if_true(access_token)
+@Then(u'user validates if the response status is {expected_code} ')
+def step_impl(context, expected_code):
+    AssertionUtilities.assert_equals(expected_code, str(RequestsBase.response_status_code()))
 
-
+@Then(u'user sends a GET request to access details')
+def step_impl(context):
+    access_token = context.access_token.json()["access_token"]
+    context.feature.users_api.oauth2_request(access_token)
+    AssertionUtilities.assert_equals('200', str(RequestsBase.response_status_code()))
 
