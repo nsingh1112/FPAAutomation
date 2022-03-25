7855716c16165cb4a8bba38d8e9a0b5aa41bc20d
Praveen Kumar <praveen.kumara@shell.com>
2022-03-25 14:03:00 +0530

    Removed encryption decryption

--- Shell_FE_Behave_Tests/features/steps/EncryptionDecryption.py
+++ Shell_FE_Behave_Tests/features/steps/EncryptionDecryption.py
@@ -6,9 +6,11 @@ from Shell_FE_Requests_Core.Utilities.AssertionUtilities import AssertionUtiliti
 
 @When('user enter the details for password to be encrypted')
 def step_impl(context):
-    result = EncryptionDecryption.encrypt_user_creds(env="UAT", user_role="System Administrator", username="insadl@shell.com.shell-gc.staging", password="Springiscoming51@")
+    result = EncryptionDecryption.encrypt_user_creds(env="UAT", user_role="System Administrator", username="",
+                                                     password="")
     AssertionUtilities.assert_if_true(result)
+
 
 @When('user enter the details for password to be decrypted')
 def step_impl(context):
-    result = EncryptionDecryption.decrypt_user_creds(environment = "UAT", position = "System Administrator", name = "insadl@shell.com.shell-gc.staging")
+    result = EncryptionDecryption.decrypt_user_creds(environment="UAT", position="System Administrator", name="")
